bfc52dee3e050a975734c706e41a33c9a1afd75b
Marcela Clos <mars@nexiit.com>
2021-08-31 19:55:42 +0200

    deleted files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+        
+       Hello Dictionary App!!
+      </header>
+    </div>
+  );
+}
+
+export default App;
