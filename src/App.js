54dfe44fac822824b044e08c8d9dbd60cff61f5a
Marcela Clos <mars@nexiit.com>
2021-09-01 11:21:08 +0200

    deleted files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
+import logo from "./logo.png";
+import "./App.css";
 
-import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       
-        
-       Hello Dictionary App!!
+        <img src={logo} className="App-logo img-fluid" alt="logo" />
+        <h1>Welcome to Diccionary App</h1>
       </header>
     </div>
   );
